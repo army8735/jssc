5f8d5c4a6df73de8dd689f40c24d67c56e3f9927
army8735 <army8735@qq.com>
2013-03-30 11:15:20 +0800

    add col show

--- jssc.js
+++ jssc.js
@@ -33,6 +33,8 @@ define(function(require, exports) {
 		var lexer = factory.lexer(syntax),
 			tabBlank = '',
 			div = document.createElement('div'),
+			col = document.createElement('div'),
+			lastCol = 0;
 			ol = document.createElement('ol');
 		ol.start = start;
 		for(var i = 0; i < tab; i++) {
@@ -40,15 +42,30 @@ define(function(require, exports) {
 		}
 		lexer.cache(cache !== null ? cache : cacheLine);
 		function join(tokens) {
+			//列数重新计算
+			if(lexer.col() > lastCol) {
+				lastCol = lexer.col();
+				var s = [];
+				for(var i = 1; i < lastCol; i++) {
+					s.push(i % 10);
+				}
+				col.innerHTML = s.join('');
+			}
+			//渲染新的代码行
 			if(!lexer.finish() && tokens[tokens.length - 1].type() == Token.LINE) {
 				tokens.pop();
 			}
 			var df = render(tokens, tabBlank);
 			ol.appendChild(df);
-			ol.style.paddingLeft = (String(lexer.line()).length - 1) * 9 + 30 + 'px';
+			//左边距
+			var pLeft = (String(lexer.line()).length - 1) * 9 + 30 + 'px';
+			col.style.paddingLeft = pLeft;
+			ol.style.paddingLeft = pLeft;
 		}
 		join(lexer.parse(code, start));
 		div.innerHTML = '<p>' + syntax + ' code</p>';
+		col.className = 'col';
+		div.appendChild(col);
 		div.appendChild(ol);
 		div.className = 'jssc';
 		if(node.parentNode.tagName.toLowerCase() == 'pre') {
