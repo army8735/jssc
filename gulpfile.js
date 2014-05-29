4a4f1b927a9c043a399488cccc53fb0d69047f6f
army8735 <army8735@qq.com>
2014-05-29 10:51:12 +0800

    update h & spm3 compatible

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,39 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var util = require('gulp-util');
+var es = require('event-stream');
+
 var fs = require('fs');
 var path = require('path');
 
-function remove(dir) {
+function mkdir(dir) {
   if(!fs.existsSync(dir)) {
-    return;
+    var parent = path.dirname(dir);
+    mkdir(parent);
+    fs.mkdirSync(dir);
   }
-  fs.readdirSync(dir).forEach(function(f) {
-    f = dir + path.sep + f;
-    if(!fs.existsSync(f)) {
-      return;
-    }
-    var stat = fs.statSync(f);
-    if(stat.isDirectory()) {
-      remove(f);
-    }
-    else if(stat.isFile()) {
-      fs.unlinkSync(f);
-    }
-  });
-  fs.rmdirSync(dir);
-}
-function copy(dir) {
-  fs.readdirSync(dir).forEach(function(f) {
-    f = dir + path.sep + f;
-    var stat = fs.statSync(f);
-    var target = f.replace('./node_modules/homunculus/web', '.');
-    if(stat.isDirectory()) {
-      fs.mkdirSync(target);
-      copy(f);
-    }
-    else if(stat.isFile()) {
-      console.log(f, '->', target);
-      var s = fs.readFileSync(f, { encoding: 'utf-8' });
-      fs.writeFileSync(target, s, { encoding: 'utf-8' });
-    }
-  });
 }
 
-gulp.task('default', function() {
-  remove('./lexer');
-  remove('./util');
-  remove('./parser');
-  copy('./node_modules/homunculus/web');
-  remove('./parser');
+gulp.task('clean', function() {
+  return gulp.src(['./lexer/*', './util/*'])
+    .pipe(clean())
+});
+gulp.task('copy', function() {
+  return gulp.src('./node_modules/homunculus/web/**/*.js')
+    .pipe(function() {
+      return es.map(function (file, cb) {
+        var target = file.path.replace('/node_modules/homunculus/web'.replace(/\//g, path.sep), '');
+        mkdir(path.dirname(target));
+        util.log(path.relative(file.cwd, file.path), '->', path.relative(file.cwd, target));
+        var content = file._contents;
+        content = content.toString('utf-8');
+        fs.writeFileSync(target, content, { encoding: 'utf-8' });
+        cb(null, file);
+      });
+    }())
+});
+
+gulp.task('default', ['clean', 'copy'], function() {
+  return gulp.src('./parser')
+    .pipe(clean())
 });
